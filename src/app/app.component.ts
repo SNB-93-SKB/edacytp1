069f56039e71df4904f4b64a472d0a06f9071ad3
SENY BALDE <seny.balde1@unchk.edu.sn>
2024-03-26 09:59:15 +0000

    commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SimpleProduct } from './interface/produit';
+import { createProducts } from './donne/produit.generator';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'edacyTP1';
+export class AppComponent implements OnInit {
+  // title = 'edacyTP1';
+  produits!: SimpleProduct[];
+
+  ngOnInit(){
+    this.produits=createProducts(); 
+    console.log(this.produits) 
+  }
 }
---
+++ src/app/donne/produit.generator.ts
@@ -0,0 +1,22 @@
+import {faker} from "@faker-js/faker";
+import { SimpleProduct } from "../interface/produit";
+export function createProduct():SimpleProduct{
+    const hasPromo=faker.datatype.boolean();
+    return{
+        id:faker.datatype.uuid(),
+        name:faker.commerce.productName(),
+        description:faker.commerce.productDescription(),
+        image:faker.image.imageUrl(640, 480, 'technics', true),
+        price:faker.commerce.price(100,1000,2),
+        category:faker.commerce.department(),
+        promo:hasPromo,
+        promoVal:hasPromo? faker.datatype.number({min:10, max :60}): undefined,
+
+    };
+}
+export function createProducts(nombreDeProduits:number=10):SimpleProduct[]{
+    return faker.helpers.multiple(createProduct,{
+        count:nombreDeProduits
+    })
+
+}
